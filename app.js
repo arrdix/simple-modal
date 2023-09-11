c1e4f75451fc010edd8ac8c3e5ec10e7b17c54af
yudistiraardi <mail.ardix@gmail.com>
2023-09-11 10:02:21 +0700

    Update codes

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ const btnOpen = document.querySelector('.modal-btn');
 const btnCls = document.querySelector('.close-btn');
 const modOverlay = document.querySelector('.modal-overlay');
 
-    btnOpen.addEventListener('click', function() {
+btnOpen.addEventListener('click', function() {
+    modOverlay.classList.add('open-modal');
+})
 
-        modOverlay.classList.add('open-modal');
-
-    })
-
-    btnCls.addEventListener('click', function() {
-
-        modOverlay.classList.remove('open-modal');
-
-    })
+btnCls.addEventListener('click', function() {
+    modOverlay.classList.remove('open-modal');
+})
